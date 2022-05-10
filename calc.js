9c71b7924b8cfbb1463fe726bc58c14b1802d8f7
OlyaBu <burasova1991@gmail.com>
2022-05-10 22:19:37 +1000

    !

--- calc.js
+++ calc.js
@@ -3,32 +3,32 @@ function getvalue(id) {
     num = parseInt(num);
     return num;
 }
-const answer = document.getElementById('out')
+const answer = document.getElementById('out');
 
 function plus() {
-    const num1 = getvalue('num1')
-    const num2 = getvalue('num2')
+    const num1 = getvalue('num1');
+    const num2 = getvalue('num2');
     result = num1 + num2;
     answer.innerHTML = result;
 }
 
 function minus() {
-    const num1 = getvalue('num1')
-    const num2 = getvalue('num2')
-     result = num1 - num2;
+    const num1 = getvalue('num1');
+    const num2 = getvalue('num2');
+    result = num1 - num2;
     answer.innerHTML = result;
 }
 
 function multiply() {
-    const num1 = getvalue('num1')
-    const num2 = getvalue('num2')
+    const num1 = getvalue('num1');
+    const num2 = getvalue('num2');
     result = num1 * num2;
     answer.innerHTML = result;
 }
 
 function division() {
-    const num1 = getvalue('num1')
-    const num2 = getvalue('num2')
+    const num1 = getvalue('num1');
+    const num2 = getvalue('num2');
     result = num1 / num2;
     answer.innerHTML = result;
 }
